4b245e2436d4b9a45fe3aaed467599cf5f273bb2
SHIFULMAHMUD <shifulmahmudkhan@gmail.com>
2021-09-04 17:19:00 +0600

    Added Alarm Reminder

--- app/src/main/java/com/android/hilltrackdoctorfinder/activity/HomeActivity.java
+++ app/src/main/java/com/android/hilltrackdoctorfinder/activity/HomeActivity.java
@@ -2,15 +2,26 @@ package com.android.hilltrackdoctorfinder.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.hilltrackdoctorfinder.R;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button reminderButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        reminderButton=findViewById(R.id.reminderButton);
+        reminderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,ReminderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
